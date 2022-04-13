d4df31cc73b5da068c3e1110e50ce7de321b3c9e
Dennis Mader <mader.dennis@live.de>
2022-04-13 14:10:09 +0200

    change color

--- src/components/base/sw-button/sw-button.vue
+++ src/components/base/sw-button/sw-button.vue
@@ -205,7 +205,7 @@ $sw-button-transition: all 0.15s ease-out;
   }
 
   &.sw-button--primary {
-     background: $color-shopware-brand-900;
+     background: $color-shopware-brand-500;
      color: $color-white;
      line-height: 36px;
      border: 0 none;
