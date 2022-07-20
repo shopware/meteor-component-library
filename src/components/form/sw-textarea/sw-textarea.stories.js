cc9d4f07e0a30c2d09d0f4f457d15a443239bd5c
Sebastian Seggewiss <s.seggewiss@shopware.com>
2022-07-20 09:47:00 +0200

    Implement textarea

--- src/components/form/sw-textarea/sw-textarea.stories.js
+++ src/components/form/sw-textarea/sw-textarea.stories.js
@@ -6,12 +6,52 @@ export default {
   args: {
     label: 'Textareafield',
   },
+  argTypes: {
+    input: {
+      action: 'input',
+      table: {
+        category: 'Events'
+      }
+    },
+    change: {
+      action: 'change',
+      table: {
+        category: 'Events'
+      }
+    },
+    inheritanceRestore: {
+      action: 'inheritance-restore',
+      table: {
+        category: 'Events'
+      }
+    },
+    inheritanceRemove: {
+      action: 'inheritance-remove',
+      table: {
+        category: 'Events'
+      }
+    },
+    hint: {
+      control: { type: 'text' },
+    },
+  },
 };
 
 const Template = (args, { argTypes }) => ({
+  template: '<div><sw-textarea v-bind="$props" v-model="currentValue" @input="input" @change="change" @inheritance-restore="inheritanceRestore" @inheritance-remove="inheritanceRemove"><template #hint>{{ hint }}</template></sw-textarea><p style="display: none">hidden</p></div>',
   props: Object.keys(argTypes),
   components: { SwTextarea },
-  template: '<sw-textarea v-bind="$props"></sw-textarea>',
+  data() {
+    return { currentValue: '' }
+  },
+  watch: {
+    value(v) {
+      this.currentValue = v;
+    }
+  },
+  created() {
+    this.currentValue = this.value;
+  },
 });
 
 export const Default = Template.bind({});
