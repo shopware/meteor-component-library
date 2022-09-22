b5520dab22e82d8677ae7eae3f039d43e0a6eddb
Jannis Leifeld <j.leifeld@shopware.com>
2022-09-22 13:22:52 +0200

    NEXT-23195 - optimize rendering  behavior of grid

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: [
+    "js",
+    "ts",
+    "json",
+    // tell Jest to handle `*.vue` files
+    "vue"
+  ],
+  transform: {
+    // process `*.vue` files with `vue-jest`
+    ".*\\.(vue)$": "@vue/vue2-jest"
+  },
+  roots: ['src'],
+  testMatch: ['<rootDir>/src/**/*.spec.js', '<rootDir>/src/**/*.spec.ts'],
+  collectCoverage: false,
+  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"]
 };
