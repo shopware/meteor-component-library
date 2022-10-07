b236f8a921ce7c0c025843d6da7a6712b505f99a
Jannis Leifeld <j.leifeld@shopware.com>
2022-10-07 13:51:39 +0200

    NEXT-23200 - add segmented controls

--- src/components/navigation/sw-segmented-control/sw-segmented-control.stories.js
+++ src/components/navigation/sw-segmented-control/sw-segmented-control.stories.js
@@ -4,7 +4,54 @@ export default {
   title: 'Components/Navigation/sw-segmented-control',
   component: SwSegmentedControl,
   argTypes: {},
-  args: {}
+  args: {
+    disableContext: true,
+    actions: [
+      {
+        id: 'lableStart',
+        label: 'X products selected',
+        onClick: ({ checkboxValue }) => console.log('Checked Label E', checkboxValue),
+        isPressed: true,
+        hasCheckbox: true
+      },
+      'divider',
+      {
+        id: 'labelA',
+        label: 'Label A',
+        onClick: () => alert('Label A'),
+        iconName: 'regular-tag',
+        disabled: true,
+      },
+      'divider',
+      {
+        id: 'labelB',
+        label: 'Label B',
+        onClick: () => alert('Label B'),
+        isPressed: true
+      },
+      {
+        id: 'labelC',
+        label: 'Label C',
+        onClick: () => alert('Label C'),
+        isPressed: false,
+        isCritical: true
+      },
+      {
+        id: 'labelD',
+        label: 'Label D',
+        onClick: () => alert('Label D'),
+        isPressed: true,
+        isCritical: true
+      },
+      {
+        id: 'labelF',
+        label: 'Label F',
+        onClick: () => alert('Add the popover to the "options__${action.id}" slot and toggle them'),
+        iconName: 'regular-calendar',
+        options: true
+      },
+    ]
+  }
 };
 
 const Template = (args, { argTypes }) => ({
