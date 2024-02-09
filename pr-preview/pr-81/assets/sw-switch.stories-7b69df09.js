4965415bc2c7ea23f66dc6435267c617feecb59f
jleifeld <jleifeld@users.noreply.github.com>
2024-02-09 09:34:59 +0000

    Deploy preview for PR 81 🛫

---
+++ pr-preview/pr-81/assets/sw-switch.stories-7b69df09.js
@@ -0,0 +1,8 @@
+import{S as t}from"./sw-switch-b63415e5.js";import"./vue.esm-bundler-651c6785.js";import"./sw-base-field-92d7614b.js";import"./tooltip.directive-5026512a.js";import"./sw-icon-6b99fd11.js";import"./iframe-e89b9ed4.js";import"../sb-preview/runtime.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./isObject-32699ff3.js";import"./sw-field-error-d121f603.js";const y={title:"Components/Form/sw-switch",component:t,render:n=>({components:{SwSwitch:t},template:`
+      <sw-switch
+        v-bind="args"
+        @change="args.change"
+        @inheritance-remove="args.inheritanceRemove"
+      ></sw-switch>`,setup:()=>({args:n})}),argTypes:{change:{action:"change",table:{category:"Events"}},"inheritance-restore":{action:"inheritance-restore",table:{category:"Events"}},inheritanceRemove:{action:"inheritance-remove",table:{category:"Events"}},label:{control:{type:"text"}}},args:{label:"Switchfield"}},e={name:"sw-switch"};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: "sw-switch"
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const S=["DefaultStory"];export{e as DefaultStory,S as __namedExportsOrder,y as default};
