9ed299fa456d60a438d181fea8043cfac572342c
jleifeld <jleifeld@users.noreply.github.com>
2024-02-16 07:41:37 +0000

    Deploy preview for PR 81 🛫

---
+++ pr-preview/pr-81/assets/sw-loader.interactive.stories-8e26e76e.js
@@ -0,0 +1,16 @@
+import l from"./sw-loader.stories-035801b5.js";import"./sw-loader-479eb282.js";import"./vue.esm-bundler-f31f98e0.js";import"./_plugin-vue_export-helper-c27b6911.js";const L={...l,title:"Interaction Tests/Feedback Indicator/sw-loader"},e={name:"Render loader with 50px",args:{size:"50px"}},r={name:"Render loader with 90px",args:{size:"90px"}},a={name:"Render loader with 7px",args:{size:"7px"}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: "Render loader with 50px",
+  args: {
+    size: "50px"
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var n,d,i;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: "Render loader with 90px",
+  args: {
+    size: "90px"
+  }
+}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var p,c,m;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  name: "Render loader with 7px",
+  args: {
+    size: "7px"
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const T=["VisualTestLoaderAt50","VisualTestLoaderAt90","VisualTestLoaderAt7"];export{e as VisualTestLoaderAt50,a as VisualTestLoaderAt7,r as VisualTestLoaderAt90,T as __namedExportsOrder,L as default};
